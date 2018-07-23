c52be6c29f8ad3a939e010bb2e8d7c9c4b10ff53
SERGIO ANDRES VARGAS <serandvaraco@outlook.com>
2018-07-23 12:00:07 -0500

    Add Workshop changes API Shopping Cart and Demo Employee Docker

--- Workshop/Template/APIShoppingCart/APIShoppingCart/Startup.cs
+++ Workshop/Template/APIShoppingCart/APIShoppingCart/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using APIShoppingCart.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -39,10 +40,18 @@ namespace APIShoppingCart
             });
 
             services.AddMvc();
-            services.AddDbContext<Models.ShoppingCartDataContext>(o =>
-            {
-                o.UseSqlServer(@"Data source=dockerworkshop.database.windows.net;Initial Catalog=ShoppingCartDB;User Id=serandvaraco;password=Abc123..");
-            });
+
+            services.AddDbContext<Models.ShoppingCartDataContext>
+                (o =>
+                {
+                    o.UseSqlServer(
+                        @"Data source=dockerworkshop.database.windows.net;
+                          Initial Catalog=ShoppingCartDB;
+                          User Id=serandvaraco;
+                          password=Abc123..");
+                });
+
+
 
             services.Configure<MvcOptions>(options =>
             {
@@ -63,8 +72,20 @@ namespace APIShoppingCart
                 settings.GeneratorSettings.DefaultPropertyNameHandling =
                     PropertyNameHandling.CamelCase;
             });
+            /// permite crear la base de datos si no existe
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<ShoppingCartDataContext>();
+                context.Products.AddRange(new Models.Product[] {
+                    new Product { Id = 1, Name = "Product 1", Description = "Product Description 1", Price = 9 },
+                    new Product { Id = 2, Name = "Product 2", Description = "Product Description 2", Price = 9 },
+                });
 
-            
+                context.SaveChanges(); 
+                context.Database.Migrate();
+            }
+
+
             app.UseCors("ALL");
             app.UseMvc();
         }
